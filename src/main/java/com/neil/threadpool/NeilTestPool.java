95ae5ee0cfe5a43bc8bc01759adc21c8952f42dd
Neil <865380824@qq.com>
2022-07-29 10:29:53 +0800

    202207291029

--- src/main/java/com/neil/threadpool/NeilTestPool.java
+++ src/main/java/com/neil/threadpool/NeilTestPool.java
@@ -5,8 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.HashSet;
-import java.util.concurrent.BlockingDeque;
-import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
